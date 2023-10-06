b57d2fcd5a20fdb16721a4fee66f91aced4755ab
HAOlleik <hussein.olleik5@gmail.com>
2023-10-05 20:38:33 -0400

    logic functional

--- src/GameEngineDriver.cpp
+++ src/GameEngineDriver.cpp
@@ -47,20 +47,64 @@ map<string, Action> actionToString {
     {"issueorder", issue_order},
     {"endissueorders", end_issue_orders},
     {"execorder", exec_order},
-    {"endexecorder", end_exec_orders},
-    {"wins", win_game},
+    {"endexecorders", end_exec_orders},
+    {"win", win_game},
     {"play", play},
     {"end", end_game}
 };
 
+int inputCharCounter;
+
 void testGameEngineStates(){
-    State currentState = start;
+    State currentState = win;
+    State nextState;
 
     while (true) {
+        std::cout << "The current state is: ";
+        switch (currentState)
+        {
+        case 0:
+            std::cout << "Start";
+            break;
+
+        case 1:
+            std::cout << "Map Loaded";
+            break;
+
+        case 2:
+            std::cout << "Map Validated";
+            break;
+
+        case 3:
+            std::cout << "Players Added";
+            break;
+
+        case 4:
+            std::cout << "Assign Reinforcement";
+            break;
+
+        case 5:
+            std::cout << "Issue Orders";
+            break;
+
+        case 6:
+            std::cout << "Execute Orders";
+            break;
+
+        case 7:
+            std::cout << "Win";
+            break;
+        
+        default:
+            break;
+        }
+
+        std::cout << "\n";
+
         char inputtedAction[50];
-        cout << "Please insert next action\n";
-        cin >> inputtedAction;
-        int inputCharCounter = 0;
+        std::cout << "Please insert next action\n";
+        std::cin >> inputtedAction;
+        inputCharCounter = 0;
         char inputChar;
         while (inputtedAction[inputCharCounter]) {
             inputChar = inputtedAction[inputCharCounter];
@@ -68,19 +112,21 @@ void testGameEngineStates(){
             inputCharCounter++;
         }
 
-        if (actionToString[inputtedAction]) {
+        nextState = mapStateToActions[currentState][actionToString[inputtedAction]];
 
-        } else {
-            cout << "Please enter a valid action\n";
+        if (currentState == win) {
+            if (strcmp(inputtedAction, "play") == 0) {
+                currentState = nextState;
+            } else if (strcmp(inputtedAction, "end") == 0) {
+                std::cout << "Thank you for playing, Good Bye\n";
+                exit(0);
+            } else {
+                std::cout << "ERROR!\n";
+            }
+        } else if (nextState) {
+            currentState = nextState;
+        } else { // not correct action
+            std::cout << "ERROR!\n";
         }
-
-        // if (mapStateToActions[currentState].count(action) > 0) {
-        //     // Valid action, update the current state
-        //     currentState = mapStateToActions[currentState][action];
-        //     cout << "Action \"" << inputtedAction << "\" performed. Current state: " << currentState << endl;
-        // } else {
-        //     cout << "Invalid action \"" << inputtedAction << "\" for current state." << endl;
-        // }
-
     }
 };
