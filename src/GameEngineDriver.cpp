f2492debd5cf8895e0bf946cf4c439f5fc7ee91f
Hussein Olleik <87741386+HAOlleik@users.noreply.github.com>
2023-10-08 23:34:27 -0400

    Game Engine Assignment 1 (#6)
    * datastructures developed
    
    * logic functional
    
    * Engine Finalized - prereview
    
    * Finalized Engine
    
    * fixed some pointer issue
    
    * editted readme and removed unecessary file

--- src/GameEngineDriver.cpp
+++ src/GameEngineDriver.cpp
@@ -1,3 +1,38 @@
-void testGameEngineStates(){
+#include <string.h>
 
+#include "GameEngine.h"
+
+using namespace std;
+
+void testGameEngineStates()
+{
+    GameEngine newGame;
+    State nextState;
+
+    while (true)
+    {
+        cout << newGame;
+
+        char inputtedAction[50];
+        cout << "Please insert next action\n";
+        cin >> inputtedAction;
+        inputToLower(inputtedAction);
+
+        nextState = mapStateToActions[*(newGame.getState())][actionToString[inputtedAction]];
+
+        if (*(newGame.getState()) == win & (strcmp(inputtedAction, "end") == 0))
+        {
+            cout << "Thank you for playing, Good Bye\n";
+            exit(0);
+        }
+
+        if (nextState)
+        {
+            newGame.setState(&nextState);
+        }
+        else
+        {
+            cout << "ERROR!\n";
+        }
+    }
 };
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "Order.h"
 
+#include "GameEngineDriver.cpp"
+
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
-
+    testGameEngineStates();
     // startUpPhase();
     // mainPlayPhase();
     testOrdersLists();
