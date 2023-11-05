05ba91a8557f27297c2c390e6b8ad947d837e344
Aleksandr Vinokhodov <vinokhodovalex@gmail.com>
2023-11-05 01:15:37 -0400

    partial work

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -1,6 +1,6 @@
 #include "GameEngine.h"
 
-void inputToLower(char* input) // switches passed arguments to lower case
+void inputToLower(char *input) // switches passed arguments to lower case
 {
     int inputCharCounter = 0;
     char inputChar;
@@ -12,80 +12,75 @@ void inputToLower(char* input) // switches passed arguments to lower case
     }
 }
 
-GameEngine::GameEngine() { // default constructor
+GameEngine::GameEngine()
+{ // default constructor
     state = new State(start);
 }
 
-GameEngine::GameEngine(const GameEngine &g) { // copy constructor
+GameEngine::GameEngine(const GameEngine &g)
+{ // copy constructor
     (*this).state = g.state;
 }
 
-GameEngine::~GameEngine() { // destructor
+GameEngine::~GameEngine()
+{ // destructor
     std::cout << "Game engine destroyed\n";
 }
 
-std::map<State, std::map<Action, State>> mapStateToActions{ // map of possible actions from a state, and the leading state of that action
-    {start, {{load_map, map_loaded}}},
-    {map_loaded, {{load_map, map_loaded}, {validate_map, map_validated}}},
-    {map_validated, {{add_player, players_added}}},
-    {players_added, {{add_player, players_added}, {assign_countries, assign_reinforcement}}},
-    {assign_reinforcement, {{issue_order, issue_orders}}},
-    {issue_orders, {{issue_order, issue_orders}, {end_issue_orders, execute_orders}}},
-    {execute_orders, {{exec_order, execute_orders}, {end_exec_orders, assign_reinforcement}, {win_game, win}}},
-    {win, {{play, start}}}};
-
-std::map<std::string, Action> actionToString{ // map between string and action to be able to compare values
-    {"loadmap", load_map},
-    {"validatemap", validate_map},
-    {"addplayer", add_player},
-    {"assigncountries", assign_countries},
-    {"issueorder", issue_order},
-    {"endissueorders", end_issue_orders},
-    {"execorder", exec_order},
-    {"endexecorders", end_exec_orders},
-    {"win", win_game},
-    {"play", play},
-    {"end", end_game}};
-
-
-//assignment operator overload
-GameEngine &GameEngine::operator = (const GameEngine &g)
+// assignment operator overload
+GameEngine &GameEngine::operator=(const GameEngine &g)
 {
     (*this).state = g.state;
     return *this;
 }
 
-ostream& operator<<(ostream& os, GameEngine& gameEngine) { //insert stream operator
-    const char* currentStateString;
+ostream &operator<<(ostream &os, GameEngine &gameEngine)
+{ // insert stream operator
+    const char *currentStateString;
+    // getCommand(currentStaet:state) -> process
+    // if effect is not empty then it is an error
+    switch (*(gameEngine.getState()))
+    {
+    case 1:
+        currentStateString = "Start";
+        // 4) use the gamestart command to
+        // a) fairly distribute all the territories to the players
+        // b) determine randomly the order of play of the players in the game
+        // c) give 50 initial army units to the players, which are placed in their respective reinforcement pool
+        // d) let each player draw 2 initial cards from the deck using the deck’s draw() method
+        // e) switch the game to the play phase
+        break;
+    case 2:
+        // 1) use the loadmap <filename> command to select a map from a list of map files as stored in a directory,
+        // which results in the map being loaded in the game.
 
-    switch (*(gameEngine.getState())) {
-        case 1:
-            currentStateString = "Start";
-            break;
-        case 2:
-            currentStateString = "Map Loaded";
-            break;
-        case 3:
-            currentStateString = "Map Validated";
-            break;
-        case 4:
-            currentStateString = "Players Added";
-            break;
-        case 5:
-            currentStateString = "Assign Reinforcement";
-            break;
-        case 6:
-            currentStateString = "Issue Orders";
-            break;
-        case 7:
-            currentStateString = "Execute Orders";
-            break;
-        case 8:
-            currentStateString = "Win";
-            break;
-        default:
-            break;
+        currentStateString = "Map Loaded";
+        break;
+    case 3:
+        currentStateString = "Map Validated";
+        // 2) use the validatemap command to validate the map (i.e. it is a connected graph, etc – see assignment 1).
+        break;
+    case 4:
+        currentStateString = "Players Added";
+        // 3) use the addplayer <playername> command to enter players in the game (2-6 players)
+        break;
+    case 5:
+        currentStateString = "Assign Reinforcement";
+        break;
+    case 6:
+        currentStateString = "Issue Orders";
+        break;
+    case 7:
+        currentStateString = "Execute Orders";
+        break;
+    case 8:
+        currentStateString = "Win";
+        break;
+    default:
+        break;
     }
+
+    // command->saveEffect(newState or error: string);
 
     os << "The current state is: " << currentStateString << "\n\n";
     return os;
--- src/GameEngine.h
+++ src/GameEngine.h
@@ -1,8 +1,12 @@
 #ifndef CARD_H
 #define CARD_H
 #include <iostream>
+#include <memory>
 #include <map>
 using std::ostream;
+
+#include "Player.h"
+#include "Map.h"
 
 enum State // available states
 {
@@ -31,16 +35,33 @@ enum Action // available actions
     end_game = 11
 };
 
-extern std::map<State, std::map<Action, State>> mapStateToActions;
-extern std::map<std::string, Action> actionToString;
+std::map<State, std::map<Action, State>> mapStateToActions{// map of possible actions from a state, and the leading state of that action
+                                                           {start, {{load_map, map_loaded}}},
+                                                           {map_loaded, {{load_map, map_loaded}, {validate_map, map_validated}}},
+                                                           {map_validated, {{add_player, players_added}}},
+                                                           {players_added, {{add_player, players_added}, {assign_countries, assign_reinforcement}}},
+                                                           {assign_reinforcement, {{issue_order, issue_orders}}},
+                                                           {issue_orders, {{issue_order, issue_orders}, {end_issue_orders, execute_orders}}},
+                                                           {execute_orders, {{exec_order, execute_orders}, {end_exec_orders, assign_reinforcement}, {win_game, win}}},
+                                                           {win, {{play, start}}}};
+
+std::map<std::string, Action> actionToString{// map between string and action to be able to compare values
+                                             {"loadmap", load_map},
+                                             {"validatemap", validate_map},
+                                             {"addplayer", add_player},
+                                             {"assigncountries", assign_countries},
+                                             {"issueorder", issue_order},
+                                             {"endissueorders", end_issue_orders},
+                                             {"execorder", exec_order},
+                                             {"endexecorders", end_exec_orders},
+                                             {"win", win_game},
+                                             {"play", play},
+                                             {"end", end_game}};
 
 void inputToLower(char *input); // free function to change to inputted action to lower case
 
 class GameEngine
 {
-private:
-    State *state;
-
 public:
     GameEngine();                                         // default
     GameEngine(const GameEngine &g);                      // copy constr
@@ -48,6 +69,12 @@ public:
     ~GameEngine();                                        // destructor
     State *getState() { return state; };                  // getter for state
     void setState(State *newState) { state = newState; }; // setter for state
+    void startupPhase();                                  // TBD
+
+private:
+    State *state;
+    std::vector<shared_ptr<Player>> _player;
+    std::unique_ptr<Map> _map;
 };
 
 ostream &
