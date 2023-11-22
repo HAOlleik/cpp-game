30ba9b35a1b15f73dd90c19dab322377a6c454be
HAOlleik <hussein.olleik5@gmail.com>
2023-11-22 10:44:26 -0500

    Fixed some logic

--- src/Order.cpp
+++ src/Order.cpp
@@ -298,7 +298,7 @@ bool BombOrder::validate()
 	bool isAdjacent = 0;
 
 	// check if the target is not adjacent to any of the current players territories
-	for (Territory *territoryPtr : currentPlayer->getTerritories())
+	for (auto &territoryPtr : currentPlayer->getTerritories())
 	{
 		// auto it = std::find(territoryPtr->getAdjacentTerritories().begin(), territoryPtr->getAdjacentTerritories().end(), target);
 		// if (!(it == territoryPtr->getAdjacentTerritories().end()))
